0cdcec76eb0f06f174e538f282459c32ee7f02d3
nikolasstow <nikolasstow@gmail.com>
2019-07-03 18:52:59 +0000

    Section 5

--- tssandbox2/demo.js
+++ tssandbox2/demo.js
@@ -1,107 +1,30 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Person = /** @class */ (function () {
-    function Person(firstName, lastName, dateOfBirth) {
-        this.testVar = false;
-        this.FirstName = firstName;
-        this.LastName = lastName;
-        this.DateOfBirth = dateOfBirth;
+var Point = /** @class */ (function () {
+    function Point(x, y) {
+        this.X = x;
+        this.Y = y;
     }
-    Object.defineProperty(Person.prototype, "FullName", {
-        get: function () {
-            return this.FirstName + " " + this.LastName;
-        },
-        set: function (name) {
-            // set up validation
-            if (name.split(" ").length != 2)
-                throw "Invalid Name";
-            this.FirstName = name.split(' ')[0];
-            this.LastName = name.split(' ')[1];
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Person.prototype.show = function () {
-        console.log(this);
+    Point.prototype.distanceFromOrigin = function () {
+        return Math.sqrt(Math.pow(this.X, 2) + Math.pow(this.Y, 2));
     };
-    return Person;
+    Point.prototype.distanceFromPoint = function (point) {
+        return Math.sqrt(Math.pow(point.X - this.X, 2) + Math.pow(point.Y - this.Y, 2));
+    };
+    return Point;
 }());
-var Person2 = /** @class */ (function () {
-    function Person2(FirstName, LastName, DateOfBirth) {
-        this.FirstName = FirstName;
-        this.LastName = LastName;
-        this.DateOfBirth = DateOfBirth;
+var pt1 = new Point(10, 20);
+var pt2 = new Point(234, 54);
+console.log(pt1.distanceFromOrigin());
+console.log(pt1.distanceFromPoint(pt2));
+var add = function (a, b) {
+    return b + a;
+};
+console.log(add(1, 2));
+var Demo = /** @class */ (function () {
+    function Demo() {
     }
-    Person2.prototype.show = function () {
-        console.log(this);
+    Demo.CalculateAndPrint = function (m) {
+        console.log(m(10, 20));
     };
-    return Person2;
+    return Demo;
 }());
-var Employee = /** @class */ (function (_super) {
-    __extends(Employee, _super);
-    function Employee(firstName, lastName, dateOfBirth, departmentName, salary) {
-        var _this = _super.call(this, firstName, lastName, dateOfBirth) || this;
-        _this.DepartmentName = departmentName;
-        _this.Salary = salary;
-        return _this;
-    }
-    Employee.prototype.show = function () {
-        _super.prototype.show.call(this);
-        console.log(this.FullName);
-    };
-    return Employee;
-}(Person));
-var p = new Employee("Nikolas", "Stow", new Date(1997, 1, 11), "Music", 300001);
-p.FullName = "Jesse Lacey";
-p.show();
-var e;
-if (p instanceof Employee)
-    e = p;
-/*--------------------------------------*/
-var Figure = /** @class */ (function () {
-    function Figure(Dimension) {
-        this.Dimension = Dimension;
-    }
-    return Figure;
-}());
-var Square = /** @class */ (function (_super) {
-    __extends(Square, _super);
-    function Square() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    Square.prototype.Area = function () {
-        return Math.pow(this.Dimension, 2);
-    };
-    Square.prototype.Perimeter = function () {
-        return 4 * this.Dimension;
-    };
-    return Square;
-}(Figure));
-var Circle = /** @class */ (function (_super) {
-    __extends(Circle, _super);
-    function Circle() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    Circle.prototype.Area = function () {
-        return Math.PI * Math.pow(this.Dimension, 2);
-    };
-    Circle.prototype.Perimeter = function () {
-        return 2 * Math.PI * this.Dimension;
-    };
-    return Circle;
-}(Figure));
-var square = new Square(2);
-console.log(square.Area());
-var circle = new Circle(2);
-console.log(circle.Perimeter());
+Demo.CalculateAndPrint(add);
--- tssandbox2/demo.ts
+++ tssandbox2/demo.ts
@@ -1,89 +1,46 @@
-class Person {
-    FirstName: string;
-    LastName: string;
-    DateOfBirth: Date;
-    testVar: boolean = false;
-    
-    constructor(firstName: string, lastName: string, dateOfBirth: Date) {
-        this.FirstName = firstName;
-        this.LastName = lastName;
-        this.DateOfBirth = dateOfBirth;
+interface IPoint {
+    X: number;
+    Y: number;
+    Z?: number;
+    distanceFromOrigin() : number;
+    distanceFromPoint(point: IPoint): number;
+}
+
+class Point implements IPoint {
+    X: number;
+    Y: number;
+    constructor(x:number, y:number) {
+        this.X = x;
+        this.Y = y;
     }
-    
-    public get FullName():string {
-        return this.FirstName + " " + this.LastName;
+    distanceFromOrigin(): number {
+        return Math.sqrt(Math.pow(this.X,2) + Math.pow(this.Y,2));
     }
-    
-    public set FullName(name: string) {
-        // set up validation
-        if(name.split(" ").length != 2) throw "Invalid Name";
-        this.FirstName = name.split(' ')[0];
-        this.LastName = name.split(' ')[1];
-    }
-    
-    show() {
-        console.log(this)
+    distanceFromPoint(point: IPoint): number {
+         return Math.sqrt(Math.pow(point.X - this.X,2) + Math.pow(point.Y - this.Y,2));
     }
 }
 
-class Person2 {
-    constructor(private FirstName: string, private LastName: string, private DateOfBirth: Date) {
-    }
-    show() {
-        console.log(this)
+let pt1: Point = new Point(10,20);
+let pt2: Point = new Point(234,54);
+
+console.log(pt1.distanceFromOrigin());
+console.log(pt1.distanceFromPoint(pt2));
+
+interface IMath {
+    (n1: number, n2: number): number;
+}
+
+let add: IMath = function (a,b) {
+    return b + a;
+}
+
+console.log(add(1,2));
+
+class Demo {
+    static CalculateAndPrint(m: IMath) {
+        console.log(m(10,20));
     }
 }
 
-class Employee extends Person {
-    DepartmentName: string;
-    Salary: number;
-    constructor(firstName: string, lastName: string, dateOfBirth: Date, departmentName: string, salary: number) {
-        super(firstName, lastName, dateOfBirth);
-        this.DepartmentName = departmentName;
-        this.Salary = salary;
-    }
-    show() {
-        super.show();
-        console.log(this.FullName);
-    }
-}
-
-let p:Person = new Employee("Nikolas","Stow", new Date(1997, 1, 11), "Music", 300001);
-p.FullName = "Jesse Lacey";
-p.show();
-
-let e: Employee;
-
-if (p instanceof Employee) e = p as Employee;
-
-/*--------------------------------------*/
-
-abstract class Figure {
-    constructor(protected Dimension: number) {}
-    public abstract Area(): number;
-    public abstract Perimeter(): number;
-}
-
-class Square extends Figure {
-    Area(): number {
-        return Math.pow(this.Dimension, 2);
-    }
-    Perimeter(): number {
-        return 4 * this.Dimension;
-    }
-}
-
-class Circle extends Figure {
-    Area(): number {
-        return Math.PI * Math.pow(this.Dimension, 2);
-    }
-    Perimeter(): number {
-        return 2 * Math.PI * this.Dimension;
-    }
-}
-
-let square: Figure = new Square(2);
-console.log(square.Area());
-
-let circle: Figure = new Circle(2);
-console.log(circle.Perimeter());
+Demo.CalculateAndPrint(add);
